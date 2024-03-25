e4dd1cf5c70d7eecee218189953ceae36ac13c64
Сергей Козыренко <sirozha_blr@tut.by>
2024-03-25 10:22:38 +0700

    add loading service

---
+++ src/app/services/loading.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { LoadingController, LoadingOptions } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  private readonly defauiltOptions: LoadingOptions = {
+    spinner: 'bubbles',
+    translucent: true,
+  };
+
+  constructor(private loadingController: LoadingController) {}
+
+  async present(loadingOptions: LoadingOptions) {
+    const options = {
+      ...this.defauiltOptions,
+      ...loadingOptions,
+    };
+
+    const loading = await this.loadingController.create(options);
+    await loading.present();
+
+    return loading;
+  }
+}
--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastService {
+  private readonly defauiltOptions: ToastOptions = {
+    duration: 3000,
+    translucent: true,
+  };
+
   constructor(private toastController: ToastController) {}
 
-  async presentToast(message: string, color: string, duration: number = 3000) {
-    const toast = await this.toastController.create({
-      message,
-      color,
-      duration,
-    });
+  async present(taostOptions: ToastOptions) {
+    const options = {
+      ...this.defauiltOptions,
+      ...taostOptions,
+    };
+
+    const toast = await this.toastController.create(options);
     toast.present();
 
     return toast;
